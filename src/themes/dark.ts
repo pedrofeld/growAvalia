4d27d8ce6f42e64613d83a4bd7c17caf085748b9
Pedro Feld <pedro.gael.feld@gmail.com>
2025-12-11 22:13:23 -0300

    style: personalização do tema dark

--- src/themes/dark.ts
+++ src/themes/dark.ts
@@ -2,6 +2,18 @@ import { createTheme } from "@mui/material";
 
 export const darkTheme = createTheme({
     palette: {
-        mode: 'dark'
+        mode: 'dark',
+        primary: {
+            main: '#04074c',
+            contrastText: '#FFFFFF',
+        },
+        secondary: {
+            main: '#0000',
+            contrastText: '#FFFFFF',
+        },
+        background: {
+            default: '#200303',
+            paper: '#460E0E',
+        },
     }
 })
