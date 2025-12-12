5b5f1155bc2e5aa5efbeca3a3203b9e798f46d56
Pedro Feld <pedro.gael.feld@gmail.com>
2025-12-11 22:07:31 -0300

    feat: criando o corpo da aplicação de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
+import { Box, Button, TextField } from '@mui/material';
 import ButtonAppBar from './components/Navbar';
 
 function App() {
   return (
     <>
       <ButtonAppBar />
-      <h1>Hello, world!</h1>
+      <Box sx={{paddingLeft: 2}}>
+        <h1>Fazer Login</h1>
+        <form>
+          <ul style={{ listStyleType: "none", padding: 0, gap: "1rem", display: "flex", flexDirection: "column", maxWidth: "300px" }}>
+            <li>
+              <TextField 
+                label="Nome" 
+                variant="outlined"
+                slotProps={{
+                  input: {
+                    sx: {
+                      backgroundColor: "gray.200",
+                      borderRadius: 2,
+                    },
+                  },
+                }}  
+              />
+            </li>
+            <li>
+                <TextField 
+                  label="E-mail" 
+                  variant="outlined"
+                  slotProps={{
+                    input: {
+                      sx: {
+                        backgroundColor: "gray.200",
+                        borderRadius: 2,
+                      },
+                    },
+                  }}  
+                />
+            </li>
+            <li>
+              <Button variant="contained" color='primary'>
+                LOGIN
+              </Button>
+            </li>
+          </ul>
+        </form>        
+      </Box>
     </>
   )
 }
