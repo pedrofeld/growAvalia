779a637f5225ef264b4ae4c6794f6139f8f9e329
Pedro Feld <pedro.gael.feld@gmail.com>
2025-12-11 22:15:42 -0300

    style: personalização do tema light

--- src/themes/light.ts
+++ src/themes/light.ts
@@ -2,6 +2,17 @@ import { createTheme } from "@mui/material";
 
 export const darkTheme = createTheme({
     palette: {
-        mode: 'light'
+        mode: 'light',
+        primary: {
+            light: 'blue.300',
+            main: '#04074c',
+            dark: 'blue.900',
+        },
+        secondary: {
+            main: '#ffa700',
+        },
+        background: {
+            default: '#f2f2f2',
+        },
     }
 })
